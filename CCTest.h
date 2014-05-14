03061f6eadc6da28ad5dda7b39994a2a1145b971
Giancarlos Franco Scarpetta <giancarlosfrancoscarpetta@Giancarloss-MacBook-Pro.local>
2014-05-13 19:54:36 -0500

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Giancarlos Franco Scarpetta on 5/13/14.
+//  Copyright (c) 2014 Giancarlos Franco Scarpetta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Giancarlos Franco Scarpetta on 5/13/14.
+//  Copyright (c) 2014 Giancarlos Franco Scarpetta. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
